7332d1cb5c2f37e315da60238ce3b1419d97be70
Yaguang Zhang <yaguangzhang@outlook.com>
2020-11-24 12:16:43 -0500

    Add libraries.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sun Shadow Simulator
+
+Matlab scripts for investigating sun shadow (e.g., for a given location at a given time) based on LiDAR data.
+
+## Contact
+
+* **Yaguang Zhang** | *Purdue University* | Email: ygzhang@purdue.edu
+
+## License
+
+This project is licensed under the MIT License.
---
+++ setPath.m
@@ -0,0 +1,100 @@
+% SETPATH Set up the environment for the simulation.
+%
+% Yaguang Zhang, Purdue, 11/24/2020
+
+cd(fileparts(mfilename('fullpath')));
+addpath(fullfile(pwd));
+addpath(genpath(fullfile(pwd, 'libs')));
+
+% A workaround for making Matlab R2019b work. If
+% preprocessIndianaLidarDataSet.m is in the path, it has trouble using
+% loaded functions as they are, even though R2019a works anyway.
+rmpath(fullfile(pwd, 'libs', 'lidar'));
+
+% The absolute path to the shared folder holding the data and code. Please
+% make sure it is correct for the machine to run this script.
+%  - On (quite powerful) Windows Artsy:
+absHomePathWinArtsy = ['D:\One Drive - Purdue\OneDrive - purdue.edu', ...
+    '\INDOT\SunShadowSimulator'];
+%  - Local copy on the computer cluster at Purdue:
+absHomePathLinuxCoverage = ['/home/coverage', ...
+    '/SunShadowSimulator'];
+%  - Local copy on the computer cluster at Purdue:
+absHomePathLinuxCoverageOnFrankie = ['/home/coverage/nvme/', ...
+    '/SunShadowSimulator'];
+
+% Path to LiDAR data.
+absPathLidarWinArtsy = ['D:\One Drive - Purdue\OneDrive - purdue.edu', ...
+    '\OATS\CellCoverageMapper\Lidar'];
+absPathLidarLinuxCoverage = ['/home/coverage', ...
+    '/CellCoverageMapper/Lidar'];
+absPathLidarLinuxCoverageOnFrankie = ['/home/coverage/nvme/', ...
+    '/CellCoverageMapper/Lidar'];
+
+% The absolute path to Python 3. Please make sure it is correct for the
+% machine to run this script.
+%  - On (quite powerful) Windows Artsy:
+absPythonPathWinArtsy ...
+    = ['C:\Users\Yaguang Zhang\AppData\Local\Programs', ...
+    '\Python\Python37\python.exe'];
+%  - Local copy on the computer cluster at Purdue:
+absPythonPathLinuxCoverage = '/usr/bin/python3.7';
+%  - Local copy on the computer cluster at Purdue:
+absPythonPathLinuxCoverageOnFrankie = '/usr/bin/python3.7';
+
+unknownComputerErrorMsg = ...
+    ['Compute not recognized... \n', ...
+    '    Please update setPath.m for your machine. '];
+unknownComputerErrorId = 'setPath:computerNotKnown';
+
+[~, curHostname] = system('hostname');
+switch strtrim(curHostname)
+    case 'Artsy'
+        % ZYG's lab desktop.
+        ABS_PATH_TO_SHARED_FOLDER = absHomePathWinArtsy;
+        ABS_PATH_TO_LIDAR = absPathLidarWinArtsy;
+        ABS_PATH_TO_PYTHON = absPythonPathWinArtsy;
+    case 'coverage-compute-big'
+        % The computer cluster at Purdue.
+        ABS_PATH_TO_SHARED_FOLDER = absHomePathLinuxCoverage;
+        ABS_PATH_TO_LIDAR = absPathLidarLinuxCoverage;
+        ABS_PATH_TO_PYTHON = absPythonPathLinuxCoverage;
+    case 'ygzhang'
+        % The virtual machine coverage on Purdue GPU cluster Frankie.
+        ABS_PATH_TO_SHARED_FOLDER = absHomePathLinuxCoverageOnFrankie;
+        ABS_PATH_TO_LIDAR = absPathLidarLinuxCoverageOnFrankie;
+        ABS_PATH_TO_PYTHON = absPythonPathLinuxCoverageOnFrankie;
+    otherwise
+        error(unknownComputerErrorId, unknownComputerErrorMsg);
+end
+
+% We need Python for concurrent HTTP requests to get elevation data from
+% USGS faster. Make sure Python and its lib folder is added to path.
+
+% Make sure Python is available.
+curPythonVersion = pyversion;
+if isempty(curPythonVersion) || (~strcmp(curPythonVersion(1:3), '3.7'))
+    pyversion(ABS_PATH_TO_PYTHON);
+end
+% Check the version again.
+curPythonVersion = pyversion;
+if ~strcmp(curPythonVersion(1:3), '3.7')
+    error(['Loaded Python is not version 3.7.', ...
+        ' Please restart Matlab and try again!']);
+end
+% Make sure our Python module is available.
+try
+    py_addpath(fullfile(pwd, 'libs', 'python'));
+catch err
+    warning(['Error identifier: ', err.identifier]);
+    warning(['Error message: ',err.message]);
+    errorMsg = 'Unable to set Python path! ';
+    if isunix
+        errorMsg = [errorMsg, ...
+            'Please make sure both python3.7 and ', ...
+            'python3.7-dev are installed!'];
+    end
+    error(errorMsg);
+end
+
+% EOF
