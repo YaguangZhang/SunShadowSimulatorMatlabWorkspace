9c5e42ad49f6cd1e6f237f9370df99e14bb5a7d9
Yaguang Zhang <yaguangzhang@outlook.com>
2021-03-17 12:13:45 -0400

    Improve LiDAR preview figures.

--- libs/tests/debugLidarDataForAreaOfInterest.m
+++ libs/tests/debugLidarDataForAreaOfInterest.m
@@ -35,17 +35,21 @@ curZLim = zlim; newZLim = [min(curZLim(1), 0) max(curZLim(2), 0)];
 zlim(newZLim); view(2);
 title('LiDAR z on Map');
 
+boolsNotInfZ = ~isinf(simState.gridLidarZs);
 figure; hold on;
-plot3k([simConfigs.gridLatLonPts(:,2), simConfigs.gridLatLonPts(:,1), ...
-    simState.gridLidarZs-min(simState.gridLidarZs)]);
+plot3k([simConfigs.gridLatLonPts(boolsNotInfZ,2), ...
+    simConfigs.gridLatLonPts(boolsNotInfZ,1), ...
+    simState.gridLidarZs(boolsNotInfZ) ...
+    - min(simState.gridLidarZs(boolsNotInfZ))]);
 plot_google_map('MapType', 'satellite');
 curZLim = zlim; newZLim = [min(curZLim(1), 0) max(curZLim(2), 0)];
 zlim(newZLim); view(2);
 title('LiDAR z (minus min(zs)) on Map');
 
 figure; hold on;
-plot3k([simConfigs.gridLatLonPts(:,2), simConfigs.gridLatLonPts(:,1), ...
-    simState.gridLidarZs-simState.gridEles]);
+plot3k([simConfigs.gridLatLonPts(boolsNotInfZ,2), ...
+    simConfigs.gridLatLonPts(boolsNotInfZ,1), ...
+    simState.gridLidarZs(boolsNotInfZ)-simState.gridEles(boolsNotInfZ)]);
 plot_google_map('MapType', 'satellite');
 curZLim = zlim; newZLim = [min(curZLim(1), 0) max(curZLim(2), 0)];
 zlim(newZLim); view(2);
